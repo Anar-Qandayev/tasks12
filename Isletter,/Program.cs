50b0bcfb347f07115f0b80c513b4ff0c74f302cd
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-09 00:12:29 +0400

    commit

---
+++ Isletter,/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Isletter_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Anar bas dasw m edrwq";
+            bool a = HasLetter(text, "m");
+            Console.WriteLine(a);
+        }
+        static bool HasLetter(string text, string word)
+        {
+            bool isLetter = false;
+            if (Regex.IsMatch(text, @"\b" + word + @"\b"))
+            {
+                isLetter = true;
+            }
+            return isLetter;
+        }
+    }
+}
